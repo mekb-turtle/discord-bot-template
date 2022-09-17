dd634d497e4ff86448658c1421a59631da15b9c8
mekb the turtle <mekbgithub@icloud.com>
2022-09-18 00:14:15 +1000

    add slash command

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 console.log("Loading...");
 
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, SlashCommandBuilder, Routes, Interaction } from "discord.js";
+import { REST } from "@discordjs/rest";
 require("dotenv").config();
-const client = new Client({ intents: [ GatewayIntentBits.Guilds ] });
-client.once("ready", () => {
+const client: Client = new Client({ intents: [ GatewayIntentBits.Guilds ] });
+const commands = [
+	new SlashCommandBuilder().setName("ping").setDescription("Ping!"),
+].map(command => command.toJSON());
+client.once("ready", async () => {
 	console.log("Ready!");
+	const rest = new REST({ version: "10" }).setToken(client.token);
+	rest.put(Routes.applicationGuildCommands(client.user.id, "1234567890123456789"), { body: commands }).catch(console.error);
+	client.on("interactionCreate", async (i: Interaction) => {
+		if (i.isCommand()) {
+			switch (i.commandName) {
+				case "ping":
+					i.reply({ content: "Pong!", ephemeral: true });
+					break;
+			}
+		}
+	});
 });
 client.login(process.env.TOKEN);
